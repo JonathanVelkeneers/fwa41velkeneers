7cfd18d52243796b1c774d05c700c2ecae51fa2d
Jonathan Velkeneers <jonathanvelkeneers@hotmail.com>
2021-10-27 09:59:15 -0500

    Add counter

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let value = 0;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    value++;
+    res.send(`User accesses are: ${value}`);
 });
 
 module.exports = router;
